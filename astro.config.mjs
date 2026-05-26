4d28ab9f96203db73f0b99952819bc98e3e0f0f5
Himawari Blog <haayho331@gmail.com>
2026-05-26 11:07:05 +0900

    Refactor: deploy to my-astro-app directory in gh-pages branch

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://haayho46-rgb.github.io/my-astro-app/',
+  site: 'https://haayho46-rgb.github.io',
   base: '/my-astro-app/',
 });
